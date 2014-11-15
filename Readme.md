8b90563363dd01e928a6da0bf69c1f0798634207
Mustafa Saeed Haji Ali <me@mustafasha.com>
2014-11-15 15:19:36 +0300

    Initial commit - extract code from test app and created a README containing documentation.

---
+++ Readme.md
@@ -0,0 +1,208 @@
+# ZAPI
+
+ZAPI is an unofficial Zaad API.
+
+## Why?
+
+Zaad is a mobile payment product operated by Telesom in Somaliland. It allows users to send money and makes payments via
+their mobile phone. Telesom has also provided a web interface for users to manage their Zaad account.
+
+Telesom however does not provide an API for users to interact with their Zaad account. ZAPI fills this gap.
+
+## How?
+
+ZAPI essentially just does what a regular user would do when using the Zaad web interface. All operations by ZAPI simply
+send GET or POST requests to the ZAAD web interface.
+
+To use ZAPI, one requires their ZAAD account to have access to the web interface which is granted to users by Telesom
+upon request.
+
+Each operation done via ZAPI return a state object which contains information regarding the ZAPI's current session on
+the Zaad web interface. You'll need to provide this state object to subsequent requests so that ZAPI can simulate the
+experience of using the Zaad web interface.
+
+## ZAPI Operations
+
+Currently ZAPI supports the following operations:
+
+ - getCaptcha
+ - login (requires state and `getCaptcha` to have succeeded previously)
+ - submitToken (requires state and `login` to have succeeded previously)
+ - logout
+ - getBalance (requires state and `submitToken` to have succeeded previously)
+ - getRecentTransactions (requires state and `submitToken` to have succeeded previously)
+ - sendMoney (requires state and `submitToken` to have succeeded previously)
+ - sendMoneyConfirm (requires state and `sendMoney` to have succeeded previously)
+
+### getCaptcha(state, cb)
+
+This operation is used to fetch the captcha from the Zaad web interface.
+
+The `state` argument should be the last state object received from a previous ZAPI operation or an empty object.
+
+The `cb` argument is a function that will be called when the `getCaptcha` operation is complete. It will get 3 arguments:
+
+1. An `err` value if an error occurs.
+2. The new `state`. You should save this object for subsequent requests.
+3. A `captchaBuffer` which will be a Buffer containing the captcha image data if the operation was successful. It can be
+sent with the MIME type 'image/jpeg'.
+
+### login(credentials, state, cb)
+
+This operation is used to submit the login credentials to access the Zaad web interface.
+
+The `credentials` object should have a `phone`, `password` and `captcha` properties where the `phone` represents the
+phone number associated with the user's Zaad account, the `password` is the password the user uses to log into the Zaad
+web interface and the `captcha` being the captcha found in the last call to the `getCaptcha` operation.
+
+The `state` argument should be the last state object received from a previous ZAPI operation or an empty object.
+
+The `cb` argument is a function that will be called when the `login` operation is complete. It will get 3 arguments:
+
+1. An `err` value if an error occurs.
+2. The new `state`. You should save this object for subsequent requests.
+3. `loginSucceeded` will be a boolean that will be true if the login attempt succeeded and false otherwise.
+
+Telesom implemented two-factor authentication on the Zaad web interface. If `loginSucceeded` is true, you should receive
+a message on your phone that will contain a token. This token is used by ZAPI via the `submitToken` operation.
+
+### submitToken(token, state, cb)
+
+This operation is used to submit the token received on the phone after the `login` operation succeeds.
+
+The `token` argument should be a string containing the token received on the phone.
+
+The `state` argument should be the last state object received from a previous ZAPI operation or an empty object.
+
+The `cb` argument is a function that will be called when the `submitToken` operation is complete. It will get 3
+arguments:
+
+1. An `err` value if an error occurs.
+2. The new `state`. You should save this object for subsequent requests.
+3. `wasTokenValid` will be a boolean that will be true if the token submission succeeded and false otherwise.
+
+If `wasTokenValid` is true, ZAPI has successfully logged into the Zaad web interface. You will now be able to use the
+operations that requires the login process to have been completed successfully.
+
+### logout(state, cb)
+
+This operation is used to make ZAPI logout of the Zaad web interface.
+
+The `state` argument should be the last state object received from a previous ZAPI operation or an empty object.
+
+The `cb` argument is a function that will be called when the `logout` operation is complete. It will get 2 arguments:
+
+1. An `err` value if an error occurs.
+2. The new `state`. You should save this object for subsequent requests.
+
+### getBalance(state, cb)
+
+This operation is used to fetch the user's current balance. Before using this operation, you will have had to have
+completed the login process (which ends with a successful call of the `submitToken` operation).
+
+The `state` argument should be the last state object received from a previous ZAPI operation or an empty object.
+
+The `cb` argument is a function that will be called when the `getBalance` operation is complete. It will get 3
+arguments:
+
+1. An `err` value if an error occurs.
+2. The new `state`. You should save this object for subsequent requests.
+3. The current `balance` of the user's account (if the operation is successful).
+
+### getRecentTransactions(state, cb)
+
+This operation is used to fetch the user's last week's worth of transactions. Before using this operation, you will have
+had to have completed the login process (which ends with a successful call of the `submitToken` operation).
+
+The `state` argument should be the last state object received from a previous ZAPI operation or an empty object.
+
+The `cb` argument is a function that will be called when the `getRecentTransactions` operation is complete. It will get
+3 arguments:
+
+1. An `err` value if an error occurs.
+2. The new `state`. You should save this object for subsequent requests.
+3. An array of `transactions`. Each element will be object with the following properties:
+
+ - id: The transaction id
+ - date: The transaction date
+ - desc: The transaction description
+ - type: The transaction type
+ - ref: The reference of the transaction.
+ - debit: The debit value of the transaction.
+ - credit: The credit value of the transaction.
+ - balance: The balance of the transaction.
+
+The transactions are fetched from the "Account Activity" feature on the Zaad web interface.
+
+### sendMoney(transaction, state, cb)
+
+This operation is used to send money from the user's account to a different Zaad account. Before using this operation, you
+will have had to have completed the login process (which ends with a successful call of the `submitToken` operation).
+
+The `transaction` argument should an object representing the details of the transaction. It should have a `phone`
+property representing the Zaad account the money should be sent to and a `amount` property representing the amount of
+money to send.
+
+The `state` argument should be the last state object received from a previous ZAPI operation or an empty object.
+
+The `cb` argument is a function that will be called when the `getRecentTransactions` operation is complete. It will get
+3 arguments:
+
+1. An `err` value if an error occurs.
+2. The new `state`. You should save this object for subsequent requests.
+3. `wasTransactionSubmitted` will be a boolean that will be true if the transaction submission succeeded and false
+otherwise.
+
+If the transaction was successfully submitted; the money will not be sent to the recipient yet. A message will appear
+on the user's phone that will be used to complete the transaction using the `sendMoneyConfirm` operation.
+
+### sendMoneyConfirm(code, state, cb)
+
+This operation is used to send money from the user's account to a different Zaad account. Before using this operation,
+you will have had to have successfully called the `sendMoney` operation.
+
+The `code` argument should be message that appeared on the user's phone after successfully completeing the `sendMoney`
+operation.
+
+The `state` argument should be the last state object received from a previous ZAPI operation or an empty object.
+
+The `cb` argument is a function that will be called when the `getRecentTransactions` operation is complete. It will get
+3 arguments:
+
+1. An `err` value if an error occurs.
+2. The new `state`. You should save this object for subsequent requests.
+3. `wasTransactionConfirmed` will be a boolean that will be true if the transaction confirmation succeeded and false
+otherwise.
+
+If the transaction was successfully confirmed, a message should appear on the user's phone confirming the transaction
+(just like how all successful transactions work on Zaad).
+
+## Errors
+
+If an error occurs, most `err` values returned from the operations will be an object. However, ZAPI's session to the
+Zaad web interface times out, a string is returned the `err` value that says `"Session timed out"`.
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2014 Mustafa Saeed Haji Ali &lt;me@mustafasha.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,562 @@
+var request = require("request");
+var cheerio = require("cheerio");
+var debug = require("debug");
+var util = require("util");
+var XLS = require("xlsjs");
+
+var rootPath = "https://myaccount.zaad.net";
+var defaultPath = "/default.aspx";
+var welcomePath = "/welcome.aspx";
+var logoutPath = "/logout_transit.aspx";
+var tokenPath = "/CheckAccessKey.aspx";
+var sendMoneyPath = "/Admin/CustomerSingleTransactions.aspx?TranType=5";
+var reportsPath = "/Admin/Reports.aspx";
+var accountActivityPath = "/Admin/ShowReport.aspx";
+
+var zapi = {
+  getCaptcha: function(state, cb){
+    var log = debug("zapi:getCaptcha");
+
+    var jar = request.jar();
+
+    if(state.cookie) {
+      var cookie = request.cookie(state.cookie);
+
+      jar.setCookie(cookie, rootPath);
+    }
+
+    log("getting " + rootPath + defaultPath);
+
+    request({
+      url: rootPath + defaultPath,
+      jar: jar
+    }, function(err){
+      state.cookie = jar.getCookieString(rootPath);
+
+      if(err) {
+        return cb(err, state);
+      }
+
+      log("getting " + rootPath + welcomePath);
+
+      request.get({
+        url: rootPath + welcomePath,
+        jar: jar
+      }, function(err2, response, body){
+        state.cookie = jar.getCookieString(rootPath);
+
+        if(err2) {
+          return cb(err2, state);
+        }
+
+        var $ = cheerio.load(body);
+
+        var captchaUrl = $("#ImageVerifier1").attr("src");
+
+        state.viewStateAtCaptchaPage = $("#__VIEWSTATE").val();
+        state.eventValidationAtCaptchaPage = $("#__EVENTVALIDATION").val();
+
+        log("getting " + rootPath + '/' + captchaUrl);
+
+        request.get({
+          url: rootPath + "/" + captchaUrl,
+          jar: jar,
+          encoding: null
+        },
+        function(err3, response2, captchaBuffer){
+          state.cookie = jar.getCookieString(rootPath);
+
+          cb(err3, state, captchaBuffer);
+        });
+      });
+    });
+  },
+  login: function(credentials, state, cb){
+    var log = debug("zapi:getCaptcha");
+
+    var jar = request.jar();
+
+    if(state.cookie) {
+      var cookie = request.cookie(state.cookie);
+
+      jar.setCookie(cookie, rootPath);
+    }
+
+    var form = {
+      "__EVENTTARGET": "",
+      "__EVENTARGUMENT": "",
+      "__VIEWSTATE": state.viewStateAtCaptchaPage,
+      "__EVENTVALIDATION": state.eventValidationAtCaptchaPage,
+      "txtuid": credentials.phone,
+      "txtpwd": credentials.password,
+      "txtVerifyText": credentials.captcha,
+      "btnLogin": "Login"
+    };
+
+    log("posting " + rootPath + welcomePath);
+
+    request.post({
+      url: rootPath + welcomePath,
+      jar: jar,
+      form: form
+    }, function(err, httpResponse, body){
+      state.cookie = jar.getCookieString(rootPath);
+
+      var loginSucceeded = httpResponse.headers["location"] === "/CheckAccessKey.aspx";
+
+      cb(err, state, loginSucceeded);
+    });
+  },
+  submitToken: function(token, state, cb){
+    var log = debug("zapi:submitToken");
+
+    var jar = request.jar();
+
+    if(state.cookie){
+      var cookie = request.cookie(state.cookie);
+
+      jar.setCookie(cookie, rootPath);
+    }
+
+    log("getting " + rootPath + tokenPath);
+
+    request.get({
+      url: rootPath + tokenPath,
+      jar: jar
+    }, function(err, response, body){
+      state.cookie = jar.getCookieString(rootPath);
+
+      if(err) {
+        log("err", err);
+
+        return cb(err, state);
+      }
+
+      var $ = cheerio.load(body);
+
+      var viewState = $("#__VIEWSTATE").val();
+      var eventValidation = $("#__EVENTVALIDATION").val();
+
+      var form = {
+        "__LASTFOCUS": "",
+        "__VIEWSTATE": viewState,
+        "__EVENTTARGET": "",
+        "__EVENTVALIDATION": eventValidation,
+        "txtaccesskey": token,
+        "btnSubmit": "Submit"
+      };
+
+      log("posting " + rootPath + tokenPath);
+
+      request.post({
+        url: rootPath + tokenPath,
+        jar: jar,
+        form: form
+      }, function(err2, response2, body2){
+        state.cookie = jar.getCookieString(rootPath);
+
+        var wasTokenValid = body2.indexOf("<script>parent.location='Admin/';</script>") === 0;
+
+        cb(err, state, wasTokenValid);
+      });
+    });
+  },
+  logout: function (state, cb) {
+    var log = debug("zapi:logout");
+
+    var jar = request.jar();
+
+    if(state.cookie){
+      var cookie = request.cookie(state.cookie);
+
+      jar.setCookie(cookie, rootPath);
+    }
+
+    log("getting " + rootPath + logoutPath);
+
+    request.get({
+      url: rootPath + logoutPath,
+      jar: jar
+    }, function(err, response, body) {
+      state.cookie = jar.getCookieString(rootPath);
+
+      if (err) {
+        log("err", err);
+      }
+
+      return cb(err, state);
+    });
+  },
+  getBalance: function(state, cb){
+    var log = debug("zapi:getBalance");
+
+    var jar = request.jar();
+
+    if(state.cookie){
+      var cookie = request.cookie(state.cookie);
+
+      jar.setCookie(cookie, rootPath);
+    }
+
+    log("getting " + rootPath + sendMoneyPath);
+
+    request.get({
+      url: rootPath + sendMoneyPath,
+      jar: jar
+    }, function(err, response, body) {
+      state.cookie = jar.getCookieString(rootPath);
+
+      if (err) {
+        return cb(err, state);
+      }
+
+      if (sessionTimedOut(response)) {
+        return cb("Session timed out", state);
+      }
+
+      log("parsing balance");
+
+      var $ = cheerio.load(body);
+
+      var balanceText = $("#lblCurrentBalance").text().trim();
+
+      var balance = balanceText.substring(balanceText.indexOf(":") + 3);
+      balance = Number(balance);
+
+      cb(null, state, balance);
+    });
+  },
+  getRecentTransactions: function(state, cb){
+    var log = debug("zapi:getRecentTransactions");
+
+    if(state.accountId) {
+      fetchRecentTransactions(state.accountId, state, cb);
+    }
+    else{
+      fetchAccountId(state, function(err, newState, accountId){
+        log("fetched accountId", accountId);
+
+        if(err){
+          cb(err, newState);
+        }
+        else{
+          newState.accountId = accountId;
+
+          fetchRecentTransactions(accountId, newState, cb);
+        }
+      });
+    }
+  },
+  sendMoney: function(transaction, state, cb){
+    var log = debug("zapi:sendMoney");
+
+    var jar = request.jar();
+
+    if(state.cookie){
+      var cookie = request.cookie(state.cookie);
+
+      jar.setCookie(cookie, rootPath);
+    }
+
+    log("getting", rootPath + sendMoneyPath);
+
+    request.get({
+      url: rootPath + sendMoneyPath,
+      jar: jar
+    }, function(err, response, body){
+      state.cookie = jar.getCookieString(rootPath);
+
+      if(err){
+        return cb(err, state);
+      }
+
+      if (sessionTimedOut(response)) {
+        return cb("Session timed out", state);
+      }
+
+      var $ = cheerio.load(body);
+
+      var viewState = $("#__VIEWSTATE").val();
+      var eventValidation = $("#__EVENTVALIDATION").val();
+
+      var form = {
+        "__LASTFOCUS": "",
+        "__EVENTTARGET": "",
+        "__EVENTARGUMENT": "",
+        "__VIEWSTATE": viewState,
+        "__EVENTVALIDATION": eventValidation,
+        ddlTranType: 5,
+        txtaccountid: transaction.phone,
+        txtamount: transaction.amount,
+        ddlNet: 0,
+        txtdescription: transaction.description,
+        btnSubmit: "Submit"
+      };
+
+      log("posting", rootPath + sendMoneyPath);
+
+      request.post({
+        url: rootPath + sendMoneyPath,
+        jar: jar,
+        form: form
+      }, function(err2, response2, body2){
+        state.cookie = jar.getCookieString(rootPath);
+
+        if(err2){
+          return cb(err, state);
+        }
+
+        if (sessionTimedOut(response)) {
+          return cb("Session timed out", state);
+        }
+
+        if(response2.headers["location"] && response2.headers["location"].indexOf("/Admin/CustomerSingleTranConfirm.aspx") === 0){
+          state.confirmUrl = response2.headers["location"];
+
+          cb(null, state, true);
+        }
+        else{
+          cb(null, state, false);
+        }
+      });
+    });
+  },
+  sendMoneyConfirm: function(code, state, cb){
+    var log = debug("zapi:sendMoneyConfirm");
+
+    var jar = request.jar();
+
+    if(state.cookie){
+      var cookie = request.cookie(state.cookie);
+
+      jar.setCookie(cookie, rootPath);
+    }
+
+    var location = state.confirmUrl;
+
+    log("getting " + rootPath + location);
+
+    request.get({
+      url: rootPath + location,
+      jar: jar
+    }, function(err, response, body){
+      state.cookie = jar.getCookieString(rootPath);
+
+      if(err){
+        return cb(err, state);
+      }
+
+      if (sessionTimedOut(response)) {
+        return cb("Session timed out", state);
+      }
+
+      var $ = cheerio.load(body);
+      var viewState = $("#__VIEWSTATE").val();
+      var eventValidation = $("#__EVENTVALIDATION").val();
+
+      var form = {
+        "__VIEWSTATE": viewState,
+        "__EVENTVALIDATION": eventValidation,
+        "txtTransactionKey": code,
+        "btnSubmit": "Confirm & Approve"
+      };
+
+      log("posting " + rootPath + location);
+
+      request.post({
+        url: rootPath + location,
+        jar: jar,
+        form: form,
+        followRedirect: false
+      }, function(err2, response2, body2){
+        state.cookie = jar.getCookieString(rootPath);
+
+        if(err2){
+          return cb(err2, state);
+        }
+
+        if (sessionTimedOut(response2)) {
+          return cb("Session timed out", state);
+        }
+
+        var wasTransactionConfirmed = response2.headers["location"] && response2.headers["location"].indexOf("/Admin/ShowReport.aspx") === 0;
+
+        cb(null, state, wasTransactionConfirmed);
+      });
+    });
+  }
+};
+
+function sessionTimedOut(response){
+  return response.headers["location"] === "/Support/m_sessionexpredirect.aspx" ||
+    response.request.path === "/Support/m_sessionexpired.aspx";
+}
+
+function padWithZeroes(number, expectedNumberOfDigits){
+  var result = "";
+
+  for(var i = 0; i < expectedNumberOfDigits; i++){
+    result += "0";
+  }
+
+  result += number;
+
+  return result.slice(result.length - expectedNumberOfDigits);
+}
+
+function fetchAccountId(state, cb){
+  var log = debug("zapi:fetchAccountId");
+
+  var jar = request.jar();
+
+  if(state.cookie){
+    var cookie = request.cookie(state.cookie);
+
+    jar.setCookie(cookie, rootPath);
+  }
+
+  var qs = {
+    "PRINTRPT": "WEB_CUSTOMER_ACOUNTACTIVITY"
+  };
+
+  log("getting " + rootPath + reportsPath, qs);
+
+  request.get({
+    url: rootPath + reportsPath,
+    jar: jar,
+    qs: qs
+  }, function(err, response, body){
+    state.cookie = jar.getCookieString(rootPath);
+
+    if (err) {
+
+      return cb(err, state);
+    }
+
+    if (sessionTimedOut(response)) {
+      return cb("Session timed out", state);
+    }
+
+    log("parsing accountId");
+
+    var $ = cheerio.load(body);
+
+    var accountId = $("#rptShowParameters_ctl01_txtParameterValue").val();
+
+    cb(null, state, accountId);
+  });
+}
+
+function fetchRecentTransactions(accountId, state, cb) {
+  var log = debug("zapi:fetchRecentTransactions");
+
+  var jar = request.jar();
+
+  if(state.cookie){
+    var cookie = request.cookie(state.cookie);
+
+    jar.setCookie(cookie, rootPath);
+  }
+
+  var today = new Date();
+  var endDate = util.format("%s%s%s", today.getFullYear(), padWithZeroes(today.getMonth() + 1, 2), padWithZeroes(today.getDate(), 2));
+
+  var oneWeekAgo = new Date();
+  oneWeekAgo.setDate(today.getDate() - 6);
+  var begDate = util.format("%s%s%s", oneWeekAgo.getFullYear(), padWithZeroes(oneWeekAgo.getMonth() + 1, 2), padWithZeroes(oneWeekAgo.getDate(), 2));
+
+  var params = util.format("AccountID=%s!BegDate=%s!EndDate=%s", accountId, begDate, endDate);
+
+  var qs = {
+    "PRINTRPT": "WEB_CUSTOMER_ACOUNTACTIVITY",
+    "Params": params
+  };
+
+  log("getting", rootPath + accountActivityPath, qs);
+
+  request.get({
+    url: rootPath + accountActivityPath,
+    jar: jar,
+    qs: qs
+  }, function (err, response, body) {
+    state.cookie = jar.getCookieString(rootPath);
+
+    if (err) {
+
+      return cb(err, state);
+    }
+
+    if (sessionTimedOut(response)) {
+      return cb("Session timed out", state);
+    }
+
+    var $ = cheerio.load(body);
+
+    var viewState = $("#__VIEWSTATE").val();
+    var eventValidation = $("#__EVENTVALIDATION").val();
+
+    var form = {
+      "__EVENTTARGET": "CrystalReportViewer1",
+      "__EVENTARGUMENT": "export",
+      "__LASTFOCUS": "",
+      "__VIEWSTATE": viewState,
+      "__EVENTVALIDATION": eventValidation,
+      "exportformat": "RecordToMSExcel",
+      "isRange": "all"
+    };
+
+    log("posting", rootPath + accountActivityPath, qs);
+
+    request.post({
+      url: rootPath + accountActivityPath,
+      jar: jar,
+      qs: qs,
+      form: form,
+      encoding: null
+    }, function (err2, response2, body2) {
+      state.cookie = jar.getCookieString(rootPath);
+
+      if (err2) {
+        return cb(err2, state);
+      }
+
+      if (sessionTimedOut(response2)) {
+        return cb("Session timed out", state);
+      }
+
+      log("parsing excel sheet");
+
+      var workbook = XLS.read(body2, {type: "buffer"});
+
+      var rows = [];
+      var r = 2;
+      var nextRowFirstColumn = workbook.Sheets["Sheet1"][XLS.utils.encode_cell({c: 0, r: r})].v;
+
+      while (nextRowFirstColumn !== "totals:") {
+        if (!workbook.Sheets["Sheet1"][XLS.utils.encode_cell({c: 0, r: r})].v) {
+          throw  new Error("Shouldn't see an empty cell");
+        }
+
+        var row = {
+          id: workbook.Sheets["Sheet1"][XLS.utils.encode_cell({c: 0, r: r})].v,
+          date: workbook.Sheets["Sheet1"][XLS.utils.encode_cell({c: 1, r: r})].w,
+          desc: workbook.Sheets["Sheet1"][XLS.utils.encode_cell({c: 2, r: r})].v,
+          type: workbook.Sheets["Sheet1"][XLS.utils.encode_cell({c: 3, r: r})].v,
+          ref: workbook.Sheets["Sheet1"][XLS.utils.encode_cell({c: 4, r: r})].v,
+          debit: workbook.Sheets["Sheet1"][XLS.utils.encode_cell({c: 5, r: r})].v,
+          credit: workbook.Sheets["Sheet1"][XLS.utils.encode_cell({c: 6, r: r})].v,
+          balance: workbook.Sheets["Sheet1"][XLS.utils.encode_cell({c: 7, r: r})].v
+        };
+
+        rows.push(row);
+
+        r++;
+        nextRowFirstColumn = workbook.Sheets["Sheet1"][XLS.utils.encode_cell({c: 0, r: r})].v;
+      }
+
+      cb(null, state, rows);
+    });
+  });
+}
+
+module.exports = zapi;
